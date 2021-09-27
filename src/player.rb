a277ea8df2df061325c5eebee6bb545c3ae133fa
Rob Thornton <rob.thornton@shopify.com>
2021-09-27 14:13:14 -0400

    Add player

--- src/player.rb
+++ src/player.rb
@@ -1,1 +1,10 @@
 # frozen_string_literal: true
+
+class Player
+  attr_reader :location, :name
+
+  def initialize(location:, name:)
+    @location = location
+    @name = name
+  end
+end
--- test/player_test.rb
+++ test/player_test.rb
@@ -1,1 +1,17 @@
 # frozen_string_literal: true
+
+require 'test_helper'
+
+class PlayerTest < TestCase
+  def test_player_has_location
+    expected = 0
+    player = Player.new(name: "", location: expected)
+    assert_equal(expected, player.location, "Location should be: #{expected}")
+  end
+
+  def test_player_has_name
+    expected = "Player 1"
+    player = Player.new(name: expected, location: 0)
+    assert_equal(expected, player.name, "Name should be: #{expected}")
+  end
+end
