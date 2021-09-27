004690614e7df7f85933fb927ada208790b5a35a
Rob Thornton <rob.thornton@shopify.com>
2021-09-27 14:32:05 -0400

    Implement transition (snake or ladder)

--- src/transition.rb
+++ src/transition.rb
@@ -1,1 +1,18 @@
 # frozen_string_literal: true
+
+class Transition
+  attr_reader :to
+
+  def match?(location:)
+    from == location
+  end
+
+  private
+
+  attr_reader :from
+
+  def initialize(from:, to:)
+    @from = from
+    @to = to
+  end
+end
--- test/transition_test.rb
+++ test/transition_test.rb
@@ -1,1 +1,16 @@
 # frozen_string_literal: true
+
+require 'test_helper'
+
+class TransitionTest < TestCase
+  def test_location_matches_from
+    transition = Transition.new(from: 0, to: 1)
+    assert_equal(true, transition.match?(location: 0), 'Location matches transition')
+  end
+
+  def test_transition_to_is_expected
+    expected = 1
+    transition = Transition.new(from: 0, to: expected)
+    assert_equal(expected, transition.to, "Transitions to matches should be: #{expected}")
+  end
+end
